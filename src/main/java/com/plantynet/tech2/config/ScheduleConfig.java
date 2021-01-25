a7057096ee7e8d7741037232db3460e107a57177
KimSangWoo <tkdqja216@gmail.com>
2021-01-25 17:32:44 +0900

    실행 무리 없이는 됨

--- src/main/java/com/plantynet/tech2/config/ScheduleConfig.java
+++ src/main/java/com/plantynet/tech2/config/ScheduleConfig.java
@@ -22,6 +22,7 @@ public class ScheduleConfig
     @Value("${custom.schedule}")
     private String schedule;
     
+    //Trigger가 Job을 실행할 수 있도록 Job의 이름 ,JobDataMap 등 Job의 정보를 가지고 있는다.
     @Bean
     public JobDetail scheduleJobDetail()
     {
@@ -47,7 +48,8 @@ public class ScheduleConfig
                 .newJob(ScheduleJob.class).withIdentity("scheduleJob").storeDurably()
                 .build();
     }
-    
+    //Job을 실행시킬 조건을 가지고 있음(반복횟수, 시작시간) Scadular는 이 정보 기반으로 Job을 수행시킴
+    // 1 Trigger : 1 Job  또는 N Trigger : 1 Job  트리거 한개로 하나의 job 실행 가능  여러시간대 Trigger 로 Job 1개를 실행 가능
     @Bean
     public Trigger scheduleJobTrigger()
     {
@@ -58,8 +60,9 @@ public class ScheduleConfig
         return TriggerBuilder
                 .newTrigger().forJob(scheduleJobDetail()).withIdentity("sampleTrigger").withSchedule(scheduleBuilder)
                 .build();*/
-        
-        //cron
+    	
+        //simpleTrigger 와 Cron 방식이 있음  cron이 더 복잡한 시간설정을 할 수 있음.
+        //cron    cron 표현식 : https://www.freeformatter.com/cron-expression-generator-quartz.html
         CronScheduleBuilder cronBuilder = CronScheduleBuilder.cronSchedule(schedule);
         
         return TriggerBuilder
